3898f06ea4469df84351524d04af548cc24caba4
becrsh <15263287+becrsh@users.noreply.github.com>
2019-04-22 19:38:53 +0200

    Committing current code

---
+++ awshealth2teams/app.py
@@ -0,0 +1,39 @@
+import logging
+import pymsteams
+import os
+
+# import requests
+
+# Define A logger
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+webhook_url = os.environ.get("MSTEAMS_WEBHOOK_URL")
+
+def lambda_handler(event, context):
+    logger.info('Event: {}'.format(event))
+    notification_type = "AWS Health"
+    event_detail = event['detail']
+
+
+    # You must create the connectorcard object with the Microsoft Webhook URL
+    notification = pymsteams.connectorcard(webhook_url)
+    notification.title("{}: {} {}".format(notification_type,event_detail['eventTypeCategory'], event_detail['eventTypeCode']))
+    notification.summary(event_detail['service'])
+
+    healthevent_section = pymsteams.cardsection()
+    healthevent_section.activityTitle(event_detail['service'])
+    healthevent_section.text(event_detail['eventDescription'][0]['latestDescription'])
+    healthevent_section.addFact("Region", event['region'])
+    healthevent_section.addFact("Started on", event_detail['startTime'])
+    healthevent_section.addFact("Ended on", event_detail['endTime'])
+
+    notification.addSection(healthevent_section)
+    notification.printme()
+    notification.send()
+
+    return {
+        "message": "Function executed successfully!",
+        "event": event
+    }
+
